7598583f1c39a879fa38f18bd1e1cbd1c5a4e351
Mohit Yadav <pradipyadav82401@gmail.com>
2024-07-20 17:24:15 +0530

    All changes completed

---
+++ script.js
@@ -0,0 +1,10 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('body'),
+//     smooth: true
+// });
+
+const typed = new Typed('.typing', {
+    strings: ["Web Developer","UI/UX Designer", "Wordpress Developer","Video Editor", ],
+    typeSpeed: 50,
+    loop: true,
+  });
